8ca92bc96f0b8e299c165b1b177d167d7853f388
OCTÁVIO LEITE NETO <octavio.neto@a.ucb.br>
2023-08-12 10:23:39 -0300

    adicionando pasta teste

---
+++ teste/teste.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+float calcular_dobro(float n){
+    return n*2;
+
+}
+
+int main() {
+	
+     float n = 5.1;
+
+
+
+    printf ("%.2f", calcular_dobro);
+
+    /*
+    
+    gcc - fazer o executável
+    .\  - executar-lo
+    gcc "nome da pasta" -o "colocar o nome do executável"
+    
+   */ 
+    
+    return 0;
+}
