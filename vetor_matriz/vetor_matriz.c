f86eeee3eab91dbc09aed22cefb8d9bb6806be09
OCTÁVIO LEITE NETO <octavio.neto@a.ucb.br>
2023-08-26 10:53:59 -0300

    atividade 3 e vetor matriz atualizado

--- vetor_matriz/vetor_matriz.c
+++ vetor_matriz/vetor_matriz.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
+#define MAX_LIVROS 100
+#define TAM_NOME 100
 
-   
+int main() {
+    int opcao = 0, rodagem = 0;
+    int id[MAX_LIVROS];
+    char nome[MAX_LIVROS][TAM_NOME];
 
-    int v1[3]= {5, 11, 3};
+    while (1) {
+        printf("1-Adicionar livro\n2-Listar livros presentes na lista\n3-Sair do programa\nEscolha: ");
+        scanf("%i", &opcao);
 
+        if (opcao == 1) {
+            if (rodagem < MAX_LIVROS) {
+                printf("Informe o id do livro: ");
+                scanf("%i", &id[rodagem]);
 
-   printf ("%i %i %i", v1[0], v1[1], v1[2]); 
+                printf("Informe o nome do livro: ");
+                getchar();  // Limpar o caractere de nova linha pendente
+                fgets(nome[rodagem], TAM_NOME, stdin);
+                nome[rodagem][strcspn(nome[rodagem], "\n")] = '\0';  // Remover o caractere de nova linha
 
+                rodagem += 1;
+                system("pause");
+            } else {
+                printf("Limite de livros atingido.\n");
+            }
+        } else if (opcao == 2) {
+            for (int i = 0; i < rodagem; i++) {
+                printf("Id do livro adicionado: %i\n", id[i]);
+                printf("Nome do livro: %s\n", nome[i]);
+            }
+            system("pause");
+        } else if (opcao == 3) {
+            break;  // Encerra o programa
+        }
+    }
 
-
-
-\
     return 0;
 }
